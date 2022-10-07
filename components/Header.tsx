94998f2c0f3c696d68e4ab3b5f0cde99f41c7af3
Gabriela Bravo Morinishi <gabrielabravo.usp@gmail.com>
2022-10-07 15:41:15 -0300

    Inicial Commit - Aula 1 React

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+
+    <header>
+        <div>Logotipo</div>
+        <nav>Nevegação Menu</nav>
+        <div>Caixa de busca</div>
+
+    </header>
+  )
+}
---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function loginPage() {
+  return (
+    <> 
+    <Header/>
+    <h1> Seja Bem vindo ao React</h1>
+    </>
+  )
+}
+
